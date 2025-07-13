243b140232348c2cc060635f500a37c384ab0c6d
Nalin <nalindalal2004@gmail.com>
2025-07-13 21:35:31 +0530

    code origin

---
+++ relayer-ws/index.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data: string) {});
+});
+
--- user-ws/index.ts
+++ user-ws/index.ts
@@ -1,19 +1,33 @@
-import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 interface Room {
-    sockets: WebSocket[]
-} 
+  sockets: WebSocket[];
+}
 
-const rooms: Record<string, Room> = {}
+const rooms: Record<string, Room> = {};
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on("message", function message(data) {
+    const parsedData = JSON.parse(data);
+    if (parsedData.type == "join-room") {
+      const room = parsedData.room;
+      if (!rooms[room]) {
+        rooms[room] = {
+          sockets: [],
+        };
+      }
+      rooms[room].sockets.push(ws);
+    }
+    if (parsedData.type == "chat") {
+      const room = parsedData.room;
+      rooms[room].sockets.map((socket) => socket.send(data));
+    }
   });
 
-  ws.send('something');
+  ws.send("something");
 });
+
